a1ddf24cb83e5358ef419b4ef66ed8e19de763b4
Zameer Manji <zmanji@gmail.com>
2011-12-11 11:09:18 -0500

    Add basic structure and Compass

---
+++ compass_config.rb
@@ -0,0 +1,11 @@
+http_path = "/"
+project_path = "."
+css_dir = "output/assets/sass"
+sass_dir = "content/assets/stylesheets"
+images_dir = "output/assets/images"
+
+# SCSS all the way
+sass_options = {
+  :syntax => :scss
+}
+
