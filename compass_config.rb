012c440e6c207a65404e6255ae3cf95c5a9f4d87
Zameer Manji <zmanji@gmail.com>
2011-12-11 19:43:48 -0500

    CSS and Footer

--- compass_config.rb
+++ compass_config.rb
@@ -3,6 +3,8 @@ project_path = "."
 css_dir = "output/assets/sass"
 sass_dir = "content/assets/stylesheets"
 images_dir = "output/assets/images"
+http_images_path = "/assets/images/"
+http_stylesheets_path = "/assets/stylesheets"
 
 # SCSS all the way
 sass_options = {
