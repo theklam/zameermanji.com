0d270e1a1682c1644dbcf47b23ccd093aa082f9b
Zameer Manji <zmanji@gmail.com>
2011-12-24 23:22:59 -0500

    No more logging needed

--- lib/filters/pygments.rb
+++ lib/filters/pygments.rb
@@ -12,16 +12,12 @@ class PygmentsFilter < Nanoc3::Filter
     # highlighting. This is a code block with a class name of a programming
     # language.
 
-    log = ::Nanoc3::CLI::Logger.instance
-    log.color = true
-
     post = Hpricot(content)
     code_blocks = post.search('pre.ruby code')
     code_blocks.each do |code_block|
       code = code_block.inner_html
       code = CGI.unescapeHTML(code)
       code = ::Pygments.highlight(code, :lexer => 'ruby', :options => {:lineseparator => '<br>', :encoding => 'utf-8'})
-      log.log(:high, code)
       code_block.parent.swap code
     end
 
