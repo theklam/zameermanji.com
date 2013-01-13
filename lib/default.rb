2b52998fe0c34c2808b42a03bc396246fbb30333
Zameer Manji <zmanji@gmail.com>
2013-01-12 19:10:44 -0800

    Scrub <title> and don't tell disqus explicitly.

--- lib/default.rb
+++ lib/default.rb
@@ -5,3 +5,10 @@ include Nanoc::Helpers::LinkTo
 include Nanoc::Helpers::Capturing
 include Nanoc::Helpers::Blogging
 include Nanoc::Helpers::XMLSitemap
+
+require "loofah"
+
+# A scrubbing helper for clean :title tags
+def scrub_title(title)
+  ::Loofah.fragment(title).scrub!(:strip).text
+end
